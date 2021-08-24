51d4f3b8f5f67c0c21a1985c621d180d1f7d7266
IshaanPrince <78795543+IshaanPrince@users.noreply.github.com>
2021-08-24 15:40:06 +0530

    Add files via upload

---
+++ Horror Caves.py
@@ -0,0 +1,25 @@
+print ("horror caves")
+print("type cave1 or cave2 or cave3")
+print("you are locked in a forest.which cave would you choose to escape?")
+
+cave=input("cave:")
+
+
+
+while cave=="cave1":
+    print("you stepped into lava!game over!")
+    
+    break
+
+while cave=="cave2":
+    print("you went to a portal and teleported to your house!you won!")
+    
+    break
+
+while cave=="cave3":
+    print("you got fired by a dragon!game over!")
+    
+    break
+
+print("the end")
+print("thanks for playing!")
